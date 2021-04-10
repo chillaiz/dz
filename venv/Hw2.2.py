06077561b2fedba63b2c33d6eb271d90e8a15326
chillaiz <berlioz312b@gmail.com>
2021-04-10 20:12:35 +0600

    popravil dz

--- venv/Hw2.2.py
+++ venv/Hw2.2.py
@@ -9,12 +9,26 @@ class Complex:
         real = self.real + other.real
         img = self.img + other.img
         return Complex(real, img)
+    def __truediv__(self, other):
+        real = self.real / other.real
+        img = self.img / other.img
+        return Complex(real, img)
+    def __sub__(self, other):
+        real = self.real - other.real
+        img = self.img - other.img
+        return Complex(real, img)
+    def __mul__(self, other):
+        real = self.real * other.real
+        img = self.img * other.img
+        return Complex(real, img)
+
+
     def __str__(self):
         return  f"{self.real} + {self.img}i"
 
 c1 = Complex(1, 2)
 c2 = Complex(1, 4)
-c3 = c1 + c2
-c4 = c3 + c1
+c3 = c1 * c2
+c4 = c3 * c1
 print(c4.real)
 print(c4.img)
